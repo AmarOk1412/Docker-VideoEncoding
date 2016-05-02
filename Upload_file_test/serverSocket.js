61dd6f398c18f4d621656834c13627af9ad15b0b
DELVINQUIER Jérémy <jeremy.delvinquier@gmail.com>
2016-05-02 11:17:28 +0200

    should world ou pas

--- Upload_file_test/serverSocket.js
+++ Upload_file_test/serverSocket.js
@@ -21,34 +21,34 @@ function handler (req, res) {
 }
  
 io.sockets.on('connection', function (socket) {
-		socket.on('Start', function (data) { //data contains the variables that we passed through in the html file
-			var Name = data['Name'];
-			Files[Name] = {
-				FileSize : data['Size'],
-				Data     : "",
-				Downloaded : 0
+	socket.on('Start', function (data) { //data contains the variables that we passed through in the html file
+		var Name = data['Name'];
+		Files[Name] = {
+			FileSize : data['Size'],
+			Data     : "",
+			Downloaded : 0
+		}
+		var Place = 0;
+		try{
+			var Stat = fs.statSync('Temp/' +  Name);
+			if(Stat.isFile())
+			{
+				Files[Name]['Downloaded'] = Stat.size;
+				Place = Stat.size / 524288;
 			}
-			var Place = 0;
-			try{
-				var Stat = fs.statSync('Temp/' +  Name);
-				if(Stat.isFile())
-				{
-					Files[Name]['Downloaded'] = Stat.size;
-					Place = Stat.size / 524288;
-				}
+		}
+		catch(er){} //It's a New File
+		fs.open("Temp/" + Name, "a", 0755, function(err, fd){
+			if(err)
+			{
+				console.log(err);
 			}
-			catch(er){} //It's a New File
-			fs.open("Temp/" + Name, "a", 0755, function(err, fd){
-				if(err)
-				{
-					console.log(err);
-				}
-				else
-				{
-					Files[Name]['Handler'] = fd; //We store the file handler so we can write to it later
-					socket.emit('MoreData', { 'Place' : Place, Percent : 0 });
-				}
-			});
+			else
+			{
+				Files[Name]['Handler'] = fd; //We store the file handler so we can write to it later
+				socket.emit('MoreData', { 'Place' : Place, Percent : 0 });
+			}
+		});
 	});
 	
 	socket.on('Upload', function (data){
@@ -77,23 +77,6 @@ io.sockets.on('connection', function (socket) {
         }
     });
 	
-	socket.on('MoreData', function (data){
-		UpdateBar(data['Percent']);
-		var Place = data['Place'] * 524288; //The Next Blocks Starting Position
-		var NewFile; //The Variable that will hold the new Block of Data
-		if(SelectedFile.webkitSlice) 
-			NewFile = SelectedFile.webkitSlice(Place, Place + Math.min(524288, (SelectedFile.size-Place)));
-		else
-			NewFile = SelectedFile.mozSlice(Place, Place + Math.min(524288, (SelectedFile.size-Place)));
-		FReader.readAsBinaryString(NewFile);
-	});
-	function UpdateBar(percent){
-		document.getElementById('ProgressBar').style.width = percent + '%';
-		document.getElementById('percent').innerHTML = (Math.round(percent*100)/100) + '%';
-		var MBDone = Math.round(((percent/100.0) * SelectedFile.size) / 1048576);
-		document.getElementById('MB').innerHTML = MBDone;
-	}
-	
 	var Path = "http://localhost/";
  
 	socket.on('Done', function (data){
@@ -106,5 +89,5 @@ io.sockets.on('connection', function (socket) {
 	function Refresh(){
 		location.reload(true);
 	}
-	
+		
 });
