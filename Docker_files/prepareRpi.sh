14f47c21ae94dcf45c0d11e0451902e77613928f
mastertheif <pierre.lecorre.free@gmail.con>
2016-05-23 17:57:36 +0200

    Update_dockerfiles

--- Docker_files/prepareRpi.sh
+++ Docker_files/prepareRpi.sh
@@ -10,8 +10,8 @@ systemctl enable docker
 
 usermod -a -G docker pi
 
-wget https://bfb87ab16c3c18c716b289fef83f99937c01c202.googledrive.com/host/0B3yka9lv3tmsT0M2Y3Q2aVV2cHc/docker-volume-glusterfs -P /usr/local/bin
-wget https://bfb87ab16c3c18c716b289fef83f99937c01c202.googledrive.com/host/0B3yka9lv3tmsT0M2Y3Q2aVV2cHc/swarm -P /usr/local/bin
+wget --no-check-certificate https://bfb87ab16c3c18c716b289fef83f99937c01c202.googledrive.com/host/0B3yka9lv3tmsT0M2Y3Q2aVV2cHc/docker-volume-glusterfs -P /usr/local/bin
+wget --no-check-certificate https://bfb87ab16c3c18c716b289fef83f99937c01c202.googledrive.com/host/0B3yka9lv3tmsT0M2Y3Q2aVV2cHc/swarm -P /usr/local/bin
 chmod 755 /usr/local/bin/docker-volume-glusterfs
 chmod 755 /usr/local/bin/swarm
 
