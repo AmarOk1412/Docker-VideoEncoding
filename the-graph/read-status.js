c300f9cab200774d87ea114ddebe1abdc053ee68
DELVINQUIER Jérémy <jeremy.delvinquier@gmail.com>
2016-05-23 14:13:04 +0200

    retrieve status of containers from the Swarm

--- the-graph/read-status.js
+++ the-graph/read-status.js
@@ -7,18 +7,29 @@ class Container{
 }
 
 var ContainerList = new Array();
+  
+function getStatus(){
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', "/containers/json&all=True&size=False", true);
+	xhr.send();
+	console.log("Envoie de la requête de récupération des status.");
+	xhr.addEventListener("readystatechange", processRequest, false);
 
-$http({
-	method: 'GET',
-	url: '"/containers/json&all=True&size=False"'
-}).then(function successCallback(response) {
-	console.log("Containers status successfully retrieved, code :"+response.status);
-	var respData = JSON.parse(response.data);
-	for (var current in respData) {
-		ContainerList.push(new Container(current.Names, current.Image, current.State));
+	/*
+	 * Handler de la réception de la réponse
+	 */
+
+	function processRequest(e){
+		console.log("Le serveur a répondu a la requête de récupération des status.");
+		console.log("State : "+xhr.readyState);
+		console.log("Status : "+xhr.status);
+		if ((xhr.readyState == 4) && (xhr.status == 200)) {
+			var respData = JSON.parse(xhr.responseText);
+			for(var key in respData){
+				if (json.hasOwnProperty(key)){
+					ContainerList.push(new Container(respData[key].Names, respData[key].Image, respData[key].State));
+				}
+			}
+		}
 	}
 }
-
-  }, function errorCallback(response) {
-  	console.log("Containers status unsuccessfully retrieved, code :"+response.status);
-  });
