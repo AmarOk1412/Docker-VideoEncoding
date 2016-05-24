1469ffd3fc084764e64171a8e974b4182dd04866
Maximeservais <maximeservais@laposte.net>
2016-05-24 09:42:38 +0200

    Script split done

--- Script/ScriptSplit.sh
+++ Script/ScriptSplit.sh
@@ -7,7 +7,6 @@ probe='avprobe'
 #if fedora
 if [ -f /etc/redhat-release ]; then
 	soft='ffmpeg'
-	probe='ffprobe'
 fi
 
 #1: file, 2: split
@@ -18,20 +17,25 @@ else
 	inputFile=$(grep -Po '(?<="original_name":")[^"]*' "$1.json")
 	outputFile=$(grep -Po '(?<="wanted_name":")[^"]*' "$1.json")
 	echo inputFile : $inputFile
+	echo onputFile : $onputFile
+	
 	> $1.part
 	> "$inputFile"_key_frame.txt	
 
-	totalTime=$("$probe" -v quiet -show_format_entry duration "$inputFile")
-	#totalTime=$("$probe" -v quiet -of csv=p=0 -show_entries format=duration "$inputFile")
+	if [ -f /etc/redhat-release ]; then
+		totalTime=$(ffprobe -v quiet -of csv=p=0 -show_entries format=duration "$inputFile")
+		ffprobe -v quiet -show_frames -select_streams v -print_format json=c=1 "$inputFile" | grep -Po '("key_frame": 1).*("pkt_pts_time": ")[0-9]*\.[0-9]*' | grep -Po '(?<="pkt_pts_time": ")[0-9]*\.[0-9]*' > "$inputFile"_key_frame.txt
+	else
+		totalTime=$(avprobe -v quiet -show_entries format=duration "$inputFile"|grep -Po '(?<=duration=)[0-9]*\.[0-9]*')
+		avprobe -v quiet -show_packets -select_streams v -print_format json=c=1 "$inputFile" | grep '"flags": "K"' | grep -Po '(?<="pts_time": ")[0-9]*\.[0-9]*' > "$inputFile"_key_frame.txt	
+	fi
+	
 	echo video duration : $totalTime
 
-	"$probe" -v quiet -show_frames -select_streams v -print_format json=c=1 "$inputFile" | grep -Po '("key_frame": 1).*("pkt_pts_time": ")[0-9]*\.[0-9]*' | grep -Po '(?<="pkt_pts_time": ")[0-9]*\.[0-9]*' > "$inputFile"_key_frame.txt
-	#"$probe" -v quiet -show_frames -select_streams v -print_format json=c=1 "$inputFile" | grep -Po '("key_frame": 1).*("pkt_pts_time": ")[0-9]*\.[0-9]*' | grep -Po '(?<="pkt_pts_time": ")[0-9]*\.[0-9]*' > "$inputFile"_key_frame.txt
-	
 	sed 1d "$inputFile"_key_frame.txt -i
 
 	nbKeyFrame=$(sed -n '$=' "$inputFile"_key_frame.txt)
-	echo $nbKeyFrame
+	echo key frame number : $nbKeyFrame
 
 	if [ "$2" -le "$nbKeyFrame" ];then
 		nbKeyFrameSplit=$(($nbKeyFrame/$2))
@@ -41,22 +45,20 @@ else
 		nbSplit=$(($nbKeyFrame - 1))
 	fi
 
-	echo keyFrame number : $nbKeyFrameSplit
-
 	startTime=0
 	fileCurrentNumber=1
 
 	echo Spliting:
 	for ((i=1;i<$nbSplit;i++))
     	do
-    		endTime=$(sed -n "$(($nbKeyFrameSplit*$i)) p" "$inputFile"key_frame.txt)
+    		endTime=$(sed -n "$(($nbKeyFrameSplit*$i)) p" "$inputFile"_key_frame.txt)
 		splitTime=$(echo "$endTime-$startTime"|bc -l)
     		echo -e ${bleufonce}Split start at: $startTime
     		echo Split end at: $endTime
     		echo Split duration: $splitTime
     		echo -e Split number: $fileCurrentNumber${neutre}
 
-    		"$soft" -y -i "$1" -ss $startTime -t "$splitTime" -codec copy "$outputFile"_part_"$fileCurrentNumber".mp4
+    		"$soft" -y -i "$inputFile" -ss $startTime -t "$splitTime" -codec copy "$outputFile"_part_"$fileCurrentNumber".mp4
 		echo "file '"$outputFile"_part_"$fileCurrentNumber".mp4'" >> "$1".part
 
 		startTime=$endTime
@@ -71,7 +73,7 @@ else
     	echo Split duration: $splitTime
     	echo -e Split number: $fileCurrentNumber${neutre}
 
-	"$soft" -y -i "$1" -ss $startTime -t "$splitTime" -codec copy "$outputFile"_part_"$fileCurrentNumber".mp4
+	"$soft" -y -i "$inputFile" -ss $startTime -t "$splitTime" -codec copy "$outputFile"_part_"$fileCurrentNumber".mp4
 	echo "file '"$outputFile"_part_"$fileCurrentNumber".mp4'" >> $1.part
 
-fi
+	fi
