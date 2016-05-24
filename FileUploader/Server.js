fe15e022701cbf02869d6e7ab0437089b77271e9
AmarOk <blin.sebastien@enconn.fr>
2016-05-24 08:29:45 +0200

    for glusterfs

--- FileUploader/Server.js
+++ FileUploader/Server.js
@@ -14,7 +14,7 @@ var bitrate = '';
 
 var storage	=	multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './uploads');
+    callback(null, '/uploads');
   },
   filename: function (req, file, callback) {
     fname = 'toEncode_' + Date.now();
