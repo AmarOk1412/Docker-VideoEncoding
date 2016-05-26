8f66838edd274443ea228195ac9b59767e5f50d4
Maximeservais <maximeservais@laposte.net>
2016-05-26 11:00:49 +0200

    Script Merge updated

--- Script/ScriptMerge.sh
+++ Script/ScriptMerge.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 soft='ffmpeg'
+directory='/uploads/'
 
-#1: file, 2: split
+#1: JSON name
 if [ "$#" -ne 1 ]; then
 	echo "Illegal number of parameters"
 else
-	outputFile=$(grep -Po '(?<="wanted_name":")[^"]*' "$1.json")
-	echo outputFile : $outputFile
+	outputFile=$(grep -Po '(?<="wanted_name":")[^"]*' "$directory""$1.json")
+	if [ "$outputFile" == "$directory" ];then
+		outputFile="$directory"$(grep -Po '(?<="original_name":")[^"]*' "$1.json")	
+	fi
+	echo outputFile : "$directory"$outputFile
 
-	"$soft" -y -f concat -i $1.part -c copy "$outputFile"
+	"$soft" -y -f concat -i "$directory"$1.part -c copy "$directory""$outputFile"
 fi
